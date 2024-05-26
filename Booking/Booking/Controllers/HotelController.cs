73df0bc3580df24403bc536ab7d76ce2cda5a341
andreea-pastiu <andreea_pastiu99@yahoo.com>
2024-05-26 21:19:47 +0300

    added HotelService and HotelController and Swagger

---
+++ Booking/Booking/Controllers/HotelController.cs
@@ -0,0 +1,69 @@
+﻿using Booking.Models;
+using Booking.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Booking.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelController : ControllerBase
+    {
+        private readonly HotelService hotelService;
+        public HotelController(HotelService hotelService)
+        {
+            this.hotelService = hotelService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllHotels()
+        {
+            return Ok(hotelService.GetAllHotels());
+        }
+
+        [HttpGet("/{id}")]
+        public IActionResult GetHotelById(int id) 
+        {
+            try
+            {
+                return Ok(hotelService.GetHotelById(id));
+            }
+            catch (NullReferenceException exception)
+            {
+                return NotFound(exception.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult CreateHotel(Hotel hotel)
+        {
+            return Ok(hotelService.CreateHotel(hotel));
+        }
+
+        [HttpPut("/{id}")]
+        public IActionResult UpdateHotel(int id, Hotel updatedHotel)
+        {
+            try
+            {
+                return Ok(hotelService.UpdateHotel(id, updatedHotel));
+            }
+            catch(NullReferenceException exception)
+            {
+                return NotFound(exception.Message);
+            }
+        }
+
+        [HttpDelete("/{id}")]
+        public IActionResult DeleteHotel(int id)
+        {
+            try
+            {
+                return Ok(hotelService.DeleteHotel(id));
+            }
+            catch(NullReferenceException exception)
+            {
+                return NotFound(exception.Message);
+            }
+        }
+    }
+}
---
+++ Booking/Booking/Services/HotelService.cs
@@ -0,0 +1,62 @@
+﻿using Booking.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Booking.Services
+{
+    public class HotelService
+    {
+        private readonly BookingContext bookingContext;
+        public HotelService(BookingContext bookingContext)
+        {
+            this.bookingContext = bookingContext;
+        }
+
+        public Hotel CreateHotel(Hotel hotel)
+        {
+            bookingContext.Hotels.Add(hotel);
+            bookingContext.SaveChanges();
+            return hotel;
+        }
+
+        public List<Hotel> GetAllHotels()
+        {
+            return bookingContext.Hotels.Include(x => x.Rooms).ToList();
+        }
+
+        public Hotel GetHotelById(int id)
+        {
+            Hotel hotel = bookingContext.Hotels.Include(x => x.Rooms).FirstOrDefault(hotel => hotel.Id == id);
+            if (hotel == null)
+            {
+                throw new NullReferenceException("Hotel not found");
+            }
+            return hotel;
+        }
+
+        public Hotel UpdateHotel(int id, Hotel updatedHotel)
+        {
+            Hotel hotel = bookingContext.Hotels.Include(x => x.Rooms).FirstOrDefault(hotel => hotel.Id == id);
+            if (hotel == null)
+            {
+                throw new NullReferenceException("Hotel not found");
+            }
+            hotel.Name = updatedHotel.Name;
+            hotel.Latitude = updatedHotel.Latitude;
+            hotel.Longitude = updatedHotel.Longitude;
+            bookingContext.SaveChanges();
+            return hotel;
+        }
+
+        public Hotel DeleteHotel(int id)
+        {
+            Hotel hotel = bookingContext.Hotels.Include(x => x.Rooms).FirstOrDefault(hotel => hotel.Id == id);
+            if(hotel == null)
+            {
+                throw new NullReferenceException("Hotel not found");
+            }
+            bookingContext.Hotels.Remove(hotel);
+            bookingContext.SaveChanges();
+            return hotel;
+        }
+    }
+}
